85a16c4d549753031653616456e2c3cb9379742f
Haqqi <me@haqqi.net>
2016-10-31 11:37:42 +0700

    Add Html5Shiv asset bundle

---
+++ src/Html5ShivAsset.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace mimicreative\html5shiv;
+
+class Html5ShivAsset extends \yii\web\AssetBundle {
+  
+  /**
+  * @inherit
+  */
+  public $sourcePath = '@bower/html5shiv';
+  
+  /**
+  * @inherit
+  */
+  public $js = [
+    'dist/html5shiv.min.js',
+  ];
+  
+  public $jsOptions = [
+    'condition' => 'lte IE9',
+    'position' => \yii\web\View::POS_HEAD
+  ];
+  
+  /**
+  * Initializes the bundle.
+  * Set publish options to copy only necessary files (in this case css and font folders)
+  */
+  public function init()
+  {
+     parent::init();
+
+     $this->publishOptions['beforeCopy'] = function ($from, $to) {
+         return preg_match('%(/|\\\\)dist%', $from);
+     };
+  }
+}
+
